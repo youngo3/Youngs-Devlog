0b42bac9c68dc8f5527e5fc81ed5cde64a5f842b
youngo3 <58507488+youngo3@users.noreply.github.com>
2020-03-01 19:24:15 +0900

    'basic things'

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,10 +3,10 @@ module.exports = {
   description: `posted about 'Today I learned'`,
   author: `[youngo3]`,
   introduction: ` `,
-  siteUrl: `https://youngo3.netlify.com`, // Your blog site url
+  siteUrl: `https://young-dev.netlify.com/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `youngo3`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
@@ -15,13 +15,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'youngo3/velog', // Your repository for archive comment
+    utterances: 'youngo3/Youngs-Devlog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'JSRpuQ2',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
