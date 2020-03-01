b3d2df9583ce86758cb608300f867eacc784ec60
youngo3 <58507488+youngo3@users.noreply.github.com>
2020-03-01 19:39:15 +0900

    change theme-switch icon

--- src/components/theme-switch/index.jsx
+++ src/components/theme-switch/index.jsx
@@ -50,8 +50,8 @@ export const ThemeSwitch = () => {
           id="normal-switch"
           height={24}
           width={48}
-          checkedIcon={<div className="icon checkedIcon">D</div>}
-          uncheckedIcon={<div className="icon uncheckedIcon">L</div>}
+          checkedIcon={<div className="icon checkedIcon">🌛</div>}
+          uncheckedIcon={<div className="icon uncheckedIcon">🌞</div>}
           offColor={'#d9dfe2'}
           offHandleColor={'#fff'}
           onColor={'#999'}
